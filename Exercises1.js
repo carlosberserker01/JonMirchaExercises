709d5783eaaa71609892441099dea70dbfeb43dd
carlosberserker01 <carlosberserker01@gmail.com>
2023-04-08 01:15:33 -0600

    clean code

--- Exercises1.js
+++ Exercises1.js
@@ -2,23 +2,18 @@ const countCharOnString = (str) =>
   typeof(str) == 'string' 
     ? str.length 
     : "Value is not a string :("
-
-
 console.log(countCharOnString('Hello World!'))
 
 
 const cutString = (str, endCut) =>
   typeof(str) == 'string' && str.slice(0, endCut)
   //&& actua como ? de operador ternario pero sin else
-
 console.log(cutString('Hola Mundo', 4))
 
 
 const strToArray = str => str.split(" ")
-
 console.log(strToArray('How are you today?'))
 
 
 const repeatString = (str, times) => str.repeat(times)
-
 console.log(repeatString('Hello World! ', 3))
