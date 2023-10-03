181440f35302d06588f78606c3ac791bc03be3aa
carlosberserker01 <carlosberserker01@gmail.com>
2023-10-03 16:36:48 -0600

    CREATE dom html and JS

---
+++ js/dom.js
@@ -0,0 +1,30 @@
+// *************60. WEB APIS*************
+// console.log(window);
+// let texto = 'hola, soy tu amigo Carlos programando desde Javascript'
+// const hablar = (texto) => speechSynthesis.speak(new SpeechSynthesisUtterance(texto))
+// hablar(texto)
+
+// *************60. DOM: INTRODUCCIÓN*************
+// console.log(window.document);
+// console.log(document);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.documentElement);
+// console.log(document.doctype);
+// console.log(document.charset);
+// console.log(document.title);
+// console.log(document.links);
+// console.log(document.images);
+// console.log(document.forms);
+// console.log(document.styleSheets);
+// console.log(document.scripts);
+// setTimeout(() => {
+//   console.log(document.getSelection().toString());
+// }, 3000);
+// document.write("<h1>Hola mundo</h1>")
+
+
+
+
+// *************62. DOM: Nodos, Elementos y Selectores*************
+console.log(document.getElementsByTagName("li"));
