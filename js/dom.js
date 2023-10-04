b90c2112329679a2945ee08728ff37e924623709
carlosberserker01 <carlosberserker01@gmail.com>
2023-10-04 16:00:18 -0600

    UPDATE class 62 and 63

--- js/dom.js
+++ js/dom.js
@@ -27,4 +27,58 @@
 
 
 // *************62. DOM: Nodos, Elementos y Selectores*************
-console.log(document.getElementsByTagName("li"));
+// console.log(document.getElementsByTagName("li"));
+// console.log(document.getElementsByClassName("card"));
+// console.log(document.getElementsByName("nombre"));
+// console.log(document.getElementById("menu"));
+
+// // Con querySelector se coloca # para buscar id, . para buscar clase, o nombres de etiquetas 
+// // querySelector es mas lento que getElementById
+// console.log(document.querySelector('#menu'));
+
+// // Trae el primero
+// console.log(document.querySelector('a'));
+
+// console.log(document.querySelectorAll('a'));
+// console.log(document.querySelectorAll('a').length);
+// document.querySelectorAll('a').forEach(el => console.log(el))
+
+// console.log(document.querySelector(".card"));
+// console.log(document.querySelectorAll(".card"));
+// console.log(document.querySelectorAll(".card")[2]);
+// console.log(document.querySelector("#menu li"));
+// console.log(document.querySelectorAll("#menu li"));
+
+
+
+
+// *************63. DOM: Atributos y Data-Attributes*************
+console.log(document.documentElement.lang);
+console.log(document.documentElement.getAttribute("lang"));
+console.log(document.querySelector(".link-dom").href);
+console.log(document.querySelector(".link-dom").getAttribute("href"));
+
+document.documentElement.lang = 'es'
+console.log(document.documentElement.lang);
+document.documentElement.setAttribute("lang", "es-MX")
+console.log(document.documentElement.lang);
+
+const $linkDOM = document.querySelector(".link-dom")
+$linkDOM.setAttribute("target", "_blank")
+$linkDOM.setAttribute("rel", "noopener")
+$linkDOM.setAttribute("href", "https://www.google.com")
+console.log($linkDOM.hasAttribute("rel"));
+$linkDOM.removeAttribute("rel")
+console.log($linkDOM.hasAttribute("rel"));
+
+//Data Attributes
+console.log($linkDOM.getAttribute("data-description"));
+console.log($linkDOM.dataset);
+console.log($linkDOM.dataset.description);
+$linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento")
+console.log($linkDOM.dataset.description);
+$linkDOM.dataset.description = "Visita Google"
+console.log($linkDOM.dataset.description);
+console.log($linkDOM.hasAttribute("data-id"));
+$linkDOM.removeAttribute("data-id")
+console.log($linkDOM.hasAttribute("data-id"));
