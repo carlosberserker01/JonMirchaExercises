e32f785581607c1a3c82dbefd1351dc02e5d022a
carlosberserker01 <carlosberserker01@gmail.com>
2023-10-29 17:56:29 -0600

    UPDATE new styles / added class 65

--- js/dom.js
+++ js/dom.js
@@ -53,32 +53,87 @@
 
 
 // *************63. DOM: Atributos y Data-Attributes*************
-console.log(document.documentElement.lang);
-console.log(document.documentElement.getAttribute("lang"));
-console.log(document.querySelector(".link-dom").href);
-console.log(document.querySelector(".link-dom").getAttribute("href"));
+// console.log(document.documentElement.lang);
+// console.log(document.documentElement.getAttribute("lang"));
+// console.log(document.querySelector(".link-dom").href);
+// console.log(document.querySelector(".link-dom").getAttribute("href"));
 
-document.documentElement.lang = 'es'
-console.log(document.documentElement.lang);
-document.documentElement.setAttribute("lang", "es-MX")
-console.log(document.documentElement.lang);
+// document.documentElement.lang = 'es'
+// console.log(document.documentElement.lang);
+// document.documentElement.setAttribute("lang", "es-MX")
+// console.log(document.documentElement.lang);
 
-const $linkDOM = document.querySelector(".link-dom")
-$linkDOM.setAttribute("target", "_blank")
-$linkDOM.setAttribute("rel", "noopener")
-$linkDOM.setAttribute("href", "https://www.google.com")
-console.log($linkDOM.hasAttribute("rel"));
-$linkDOM.removeAttribute("rel")
-console.log($linkDOM.hasAttribute("rel"));
+// const $linkDOM = document.querySelector(".link-dom")
+// $linkDOM.setAttribute("target", "_blank")
+// $linkDOM.setAttribute("rel", "noopener")
+// $linkDOM.setAttribute("href", "https://www.google.com")
+// console.log($linkDOM.hasAttribute("rel"));
+// $linkDOM.removeAttribute("rel")
+// console.log($linkDOM.hasAttribute("rel"));
 
-//Data Attributes
-console.log($linkDOM.getAttribute("data-description"));
-console.log($linkDOM.dataset);
-console.log($linkDOM.dataset.description);
-$linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento")
-console.log($linkDOM.dataset.description);
-$linkDOM.dataset.description = "Visita Google"
-console.log($linkDOM.dataset.description);
-console.log($linkDOM.hasAttribute("data-id"));
-$linkDOM.removeAttribute("data-id")
-console.log($linkDOM.hasAttribute("data-id"));
+// //Data Attributes
+// console.log($linkDOM.getAttribute("data-description"));
+// console.log($linkDOM.dataset);
+// console.log($linkDOM.dataset.description);
+// $linkDOM.setAttribute("data-description", "Modelo de Objeto del Documento")
+// console.log($linkDOM.dataset.description);
+// $linkDOM.dataset.description = "Visita Google"
+// console.log($linkDOM.dataset.description);
+// console.log($linkDOM.hasAttribute("data-id"));
+// $linkDOM.removeAttribute("data-id")
+// console.log($linkDOM.hasAttribute("data-id"));
+
+
+
+
+// *************64. DOM: Estilos y Variables CSS*************
+// const $linkDOM = document.querySelector(".link-dom")
+// console.log($linkDOM.style);
+// console.log($linkDOM.getAttribute("style"));
+
+// console.log($linkDOM.style.backgroundColor);
+// console.log($linkDOM.style.color);
+// console.log(window.getComputedStyle($linkDOM));
+// console.log(getComputedStyle($linkDOM).getPropertyValue("color"));
+
+// $linkDOM.style.setProperty("text-decoration", "none")
+// $linkDOM.style.setProperty("display", "block")
+// $linkDOM.style.width = "50%"
+// $linkDOM.style.textAlign = "center"
+// $linkDOM.style.marginLeft = "auto"
+// $linkDOM.style.marginRight = "auto"
+// $linkDOM.style.padding = "1rem"
+// $linkDOM.style.borderRadius = ".5rem"
+
+// console.log("----");
+// console.log($linkDOM.style);
+// console.log($linkDOM.getAttribute("style"));
+// console.log(getComputedStyle($linkDOM));
+
+// //Variables CSS - Custom Properties CSS
+// const $html = document.documentElement,
+//   $body = document.body
+
+// let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color")
+// let varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color")
+
+// console.log(varDarkColor, varYellowColor);
+
+// $body.style.backgroundColor = varDarkColor
+// $body.style.color = varYellowColor
+
+// $html.style.setProperty("--dark-color", "#000")
+// varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color")
+
+// $body.style.setProperty("background-color", varDarkColor)
+
+// console.log(getComputedStyle($html));
+
+
+
+
+// *************65. DOM: Clases CSS *************
+const $card = document.querySelector(".card");
+console.log($card);
+console.log($card.className);
+console.log($card.classList.contains("rotate-45"));
